3cf940b1e2c712727d90813300d0a08495f36a96
Aly Khaled <alykhaled936@gmail.com>
2025-02-08 04:27:38 +0200

    create onBoarding branch and add DocLogaAndName widget

---
+++ lib/core/theming/styles.dart
@@ -0,0 +1,15 @@
+import 'package:doc_app/core/theming/colors.dart';
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const TextStyle font24Black700Weight = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+  static TextStyle font32BlueBold = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.mainBlue,
+  );
+}
--- lib/features/onboarding/widgets/doc_logo_and_name.dart
+++ lib/features/onboarding/widgets/doc_logo_and_name.dart
@@ -1,4 +1,6 @@
+import 'package:doc_app/core/theming/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 
 class DocLogoAndName extends StatelessWidget {
@@ -10,7 +12,12 @@ class DocLogoAndName extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         SvgPicture.asset(
-          'assets/images/logo.svg',
+          'assets/svgs/docdoc_logo.svg',
+        ),
+        SizedBox(width: 10.w),
+        const Text(
+          'DocDoc',
+          style: TextStyles.font24Black700Weight,
         ),
       ],
     );

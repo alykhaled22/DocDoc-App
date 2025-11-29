c0a0532a399721c621334be46c7293f2aab9ef07
ahmed <ahmed@gmail.com>
2025-11-30 00:24:20 +0200

    add ios & android splash screen

--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -3,23 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TextStyles {
-  static TextStyle font24Black700Weight = TextStyle(
-    fontSize: 24.sp,
-    fontWeight: FontWeight.w700,
-    color: Colors.black,
-  );
-  static TextStyle font32BlueBold = TextStyle(
-    fontSize: 32.sp,
-    fontWeight: FontWeight.bold,
-    color: ColorsManager.mainBlue,
-  );
-  static TextStyle font12GreyRegular = TextStyle(
-    fontSize: 12.sp,
-    color: ColorsManager.gray,
-  );
-  static TextStyle font16WhiteSemiBold = TextStyle(
-    fontSize: 16.sp,
-    fontWeight: FontWeight.w500,
-    color: Colors.white,
-  );
+  static TextStyle get font24Black700Weight => TextStyle(
+        fontSize: 24.sp,
+        fontWeight: FontWeight.w700,
+        color: Colors.black,
+      );
+
+  static TextStyle get font32BlueBold => TextStyle(
+        fontSize: 32.sp,
+        fontWeight: FontWeight.bold,
+        color: ColorsManager.mainBlue,
+      );
+
+  static TextStyle get font12GreyRegular => TextStyle(
+        fontSize: 12.sp,
+        color: ColorsManager.gray,
+      );
+
+  static TextStyle get font16WhiteSemiBold => TextStyle(
+        fontSize: 16.sp,
+        fontWeight: FontWeight.w500,
+        color: Colors.white,
+      );
 }
